dbecec1fc95780b5151b80be134523dbdea28ec9
ElenaMughi <elena.mughi@gmail.com>
2022-12-10 15:19:00 +0300

    try2

--- app/src/test/java/ru/netology/unittest/SimpleHospiceUnitTest.java
+++ app/src/test/java/ru/netology/unittest/SimpleHospiceUnitTest.java
@@ -1,9 +1,22 @@
 package ru.netology.unittest;
 
+import org.junit.After;
+import org.junit.Before;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SimpleHospiceUnitTest {
+
+    @Before
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @After
+    static void forClose() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     public void addition_isCorrect() {
